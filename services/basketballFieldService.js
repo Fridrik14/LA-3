e892dc5428315d233b04af313613985fd445d434
olafur98 <olafur.barkarson@gmail.com>
2019-10-18 21:37:51 +0000

    Fixing

---
+++ services/basketballFieldService.js
@@ -0,0 +1,13 @@
+/*
+    If the db connection is correct
+    and the mutations are correct
+    then this is the only thing left
+*/
+
+var Client = require('node-rest-client').Client;
+var remoteURL = "https://basketball-fields.herokuapp.com/api/basketball-fields";
+
+var client = new Client();
+client.get(remoteURL, function (data, response) {console.log(data);});
+    
+    
