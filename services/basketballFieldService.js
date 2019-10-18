74f4bc2a6801f6e7a544ef097035687c88731054
olafur98 <olafur.barkarson@gmail.com>
2019-10-18 22:27:04 +0000

    fixing

--- services/basketballFieldService.js
+++ services/basketballFieldService.js
@@ -2,15 +2,41 @@
     If the db connection is correct
     and the mutations are correct
     then this is the only thing left
+    https://basketball-fields.herokuapp.com/api/basketball-fields
 */
 
-var Client = require('node-rest-client').Client;
-var remoteURL = "https://basketball-fields.herokuapp.com/api/basketball-fields";
+// var Client = require('node-rest-client').Client;
+// var remoteURL = "";
 
-var client = new Client();
+// var client = new Client();
 
-client.get(remoteURL, function (data, response) {return data;});
+// const v = client.get(remoteURL, function (data, response) {return data;});
+
+
+var https = require('https');
+
+var options = {
+    host: 'basketball-fields.herokuapp.com',
+    path: '/api/basketball-fields'
+}
+var request = https.request(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
+        data += chunk;
+    });
+    res.on('end', function () {
+        console.log(data);
+
+    });
+});
+request.on('error', function (e) {
+    console.log(e.message);
+});
+request.end();
+
+
+
 module.exports = {
-    client
+    
 };
     
